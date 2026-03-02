83dbe1679b4f757bb6385396db7af89cbc1503f4
Nurhakim As'ad Wicaksono <127674012+nurhakim27@users.noreply.github.com>
2026-03-02 16:54:05 +0700

    update profile readme

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <!-- FONT END -->
 
     <!-- BOOTSTRAP & CSS START -->
-    <link href="bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet"> <!-- UNTUK ICON -->
     <link href="css/aos.css" rel="stylesheet"> <!-- UNTUK ANIMATION -->
 
